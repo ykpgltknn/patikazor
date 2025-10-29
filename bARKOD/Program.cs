fb44d0e0d75683fab6347908452074f89f0d53cc
Yakup Gültekin <124573071+ykpgltknn@users.noreply.github.com>
2025-10-29 20:06:12 +0300

    Add files via upload

---
+++ bARKOD/Program.cs
@@ -0,0 +1,12 @@
+﻿﻿namespace barkod_olusturucu_okuyucu;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Barkod barkod = new();
+
+        while (true)
+            barkod.GoreveBasla();
+    }
+}
